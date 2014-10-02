0711b64a7d71f46cc9cc910787e0c1b98b5f1816
LoreBerli <mrgorsky91@gmail.com>
2014-10-02 22:35:11 +0200

    basic modules
    director.py
    main.py

---
+++ director.py
@@ -0,0 +1,9 @@
+import map,cell,actors
+
+class gameDirector:
+    def __init__(self,mappa):
+        self.mappa = mappa
+        self.attori=[]
+        self.tagliaL=[]
+        self.albe=[]
+        self.orsi=[]
