9eedf01eb51d35134afceb430d091b5f4571e11a
LoreBerli <mrgorsky91@gmail.com>
2014-10-08 22:18:46 +0200

    added stats visualization

--- renderer.py
+++ renderer.py
@@ -1,8 +1,9 @@
 import pygame
 
 class Renderer:
-    def __init__(self, mappa):
+    def __init__(self, mappa,master):
         self.mappa=mappa
+        self.master = master
         pygame.init()
         self.surf = pygame.display.set_mode((712,512),0,32)
         self.rectSide = 8
@@ -28,13 +29,22 @@ class Renderer:
                 return (255,255,255)
         #return (255,255,255)
 
+    def drawStats(self):
+        alberi = self.master.getNumAttori()[0]
+        taglia = self.master.getNumAttori()[1]
+        tot = alberi + taglia
+        pygame.draw.line(self.surf,(0,0,0),(540,0),(540 ,710),4)
+        pygame.draw.line(self.surf,(0,0,0),(544,0),(544 ,710),4)
+        pygame.draw.line(self.surf,(51,102,0),(540,710),(540, 710-((alberi*710)/tot)),4)
+
+        pygame.draw.line(self.surf,(204,0,0),(544,0), (544 ,((taglia*710)/tot)),4)
 
 
     def draw(self):
         for i in xrange(0,self.mappa.getSize()[0]):
             for j in xrange(0,self.mappa.getSize()[1]):
                 color = self.colorOption(self.mappa.getCellActor(i,j))
-                pygame.draw.rect(self.surf,color,(i*self.rectSide,j*self.rectSide,self.rectSide,self.rectSide),0)
+                pygame.draw.rect(self.surf,color,(i*self.rectSide,j*self.rectSide,self.rectSide,self.rectSide),1)
         pygame.display.update()
 
 
